7a87a0159991ec956748e9f4a6afbfb3c2437c71
Siwon-Kim <siwonkim1108@gmail.com>
2023-06-13 15:54:41 +0900

    feat: tensorboard 추가

--- hyeseung_sentence/rnn_embedding.py
+++ hyeseung_sentence/rnn_embedding.py
@@ -6,8 +6,19 @@ from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.callbacks import Callback, LearningRateScheduler
 from tensorflow.keras import regularizers
+from tensorflow.keras.callbacks import TensorBoard
 import numpy as np
-from datas import data
+import random
+from real_data import data
+
+# TensorBoard 로그 저장 디렉토리 설정
+log_dir = "logs"
+
+# TensorBoard 콜백 정의
+tensorboard_callback = TensorBoard(log_dir=log_dir)
+
+# 데이터 섞기
+random.shuffle(data)
 
 # 데이터 분리
 symptoms, labels = zip(*data)
@@ -34,9 +45,9 @@ X_train, X_test, y_train, y_test = train_test_split(padded_symptoms, encoded_lab
 embedding_dim = 100
 model = Sequential()
 model.add(Embedding(num_words, embedding_dim, input_length=max_length))
-model.add(LSTM(46))
+model.add(LSTM(64)) # hidden layer: 조정 대상
 # model.add(LSTM(64, kernel_regularizer=regularizers.l1(0.01))) #L1 규제 적용.... 더 떨어짐ㅜ
-model.add(Dropout(0.2))
+model.add(Dropout(0.5)) # dropout - 과적합 방지: 조정 대상
 model.add(Dense(num_classes, activation='softmax'))
 
 # 모델 컴파일 (알고리즘:adam, 손실함수:categorical_crossentropy, 평가지표:accuracy)
@@ -44,10 +55,10 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 
 # 학습률 스케줄링 함수 정의 (100번동안은 학습률 유지 후 0.1씩 감소 -> 초기학습은 빠르게)
 def lr_scheduler(epoch, lr):
-    if epoch < 100:
+    if epoch < 1000:
         return lr
     else:
-        return lr * 0.1
+        return lr * 0.1 # learning rate: 조정 대상
 
 # 학습률 스케줄링 콜백 정의
 lr_scheduler_callback = LearningRateScheduler(lr_scheduler)
@@ -83,8 +94,8 @@ class CustomEarlyStopping(Callback):
 custom_early_stopping = CustomEarlyStopping(accuracy_threshold=0.95, patience=30)
 
 # 학습 (반복횟수:1000, 한번에 처리할 데이터 샘플:32)
-model.fit(X_train, y_train, epochs=1000, batch_size=32, validation_data=(X_test, y_test),
-          callbacks=[custom_early_stopping, lr_scheduler_callback], verbose=1)
+model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_test, y_test), # epochs: 조정 대상
+          callbacks=[custom_early_stopping, lr_scheduler_callback, tensorboard_callback], verbose=1)
 
 # 성능 평가
 loss, accuracy = model.evaluate(X_test, y_test)
@@ -92,7 +103,7 @@ print("테스트 손실:", loss)
 print("테스트 정확도:", accuracy)
 
 # 예측
-sample_symptom = ["오른쪽 머리 뒷 부분의 통증 발생"]
+sample_symptom = ["오른쪽 귀 뒷부분에 몽우리 손가락 한마디 정도가 만져지고 점점 커짐. 만지면 아픔. 볼부위가 많이 부었다. 발열 기침 가래 피부병변. 최근에 감기 걸린 증상이 없었다"]
 encoded_sample = tokenizer.texts_to_sequences([sample_symptom])
 padded_sample = pad_sequences(encoded_sample, maxlen=max_length, padding='post')
 prediction = model.predict(padded_sample)
