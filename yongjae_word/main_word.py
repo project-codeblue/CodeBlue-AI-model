684cdd0f8968348a12dc843491d8528e1a2c1196
backEndKwon <rhaehfdl0433@gmail.com>
2023-06-14 16:59:22 +0900

    feat: update word_model
    Former-commit-id: f7bdb9bbdac69ca67f655ec29e766a520e28564c [formerly a47d42aa2fbf80b099b9e24d3538fdf06500ea92]
    Former-commit-id: eab88b70e74478321571556695df200bc90c2a71

--- yongjae_word/main_word.py
+++ yongjae_word/main_word.py
@@ -1,66 +1,61 @@
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 # ↑상단의 주석 지우지 마세요. python이 읽는 주석입니다.
 
-#main에서 alt+g로 실행후 증상을 입력하세요. 증상 입력할때 제대로된 값을 확인하기 위해 본인이 적은 증상의 점수를 기억해주세요!
-# import time
-# import os
-# import re
-# import numpy as np
-# from pykospacing import Spacing #pip install git+https://github.com/haven-jeon/PyKoSpacing.git
+#main 사용설명서
+#alt+g로 실행후 증상을 입력하세요. 증상 입력할때 제대로된 값을 확인하기 위해 본인이 적은 증상의 점수를 기억해주세요!
+
+import numpy as np
+import re
 from konlpy.tag import Okt
 from gensim.models import FastText
 from sub_word import get_severity_level,symptom_scores
 from scipy.spatial.distance import cosine
+from stop_word import stop_words,compound_words
 
 okt = Okt()
-# spacing = Spacing()
 
 model_path = "yongjae_word/model/fasttext.model"
 vectors_ngrams_path = "yongjae_word/model/fasttext.model.wv.vectors_ngrams.npy"
 
-stopwords = ['입원','나','머','됨','됌','면서','헤','보이게','씩','증도','닮다','다','없다','고','엔','힘들다','액체','까지','두','야하다','오늘','아침','오늘아침','커지다','급격하다','치가','나타나다','조','이상승','이송이','병원','력','지속','함피','함','즉시','집중','떨어지다','하고','심하다','인','적','며','필요하다','되어다','되었다','인하다','심해지다','계속','있다','심각하다','점점','때문','을','로','상','하','지다','않다','이다','되다','에서','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다','아침','또','또한','가끔','감','코',"계,'속",'현재','이어진','상황']
-
 def preprocess(text):
-    # clean1_text = text.split()
-    # clean2_text = re.compile('[^가-힣]').sub('', clean1_text)
-    clean2_text = okt.morphs(text)
+    line = re.compile('[^가-힣]').sub(' ', text)
+    token_text = okt.morphs(line)
+    stopWords_removed_texts = [word for word in token_text if not word in stop_words]
+    compoundWords_removed_texts = [word for word in stopWords_removed_texts if not word in compound_words]
 
-    clean3_text = [word for word in clean2_text if not word in stopwords]
-        # print("main.js 1차 가공 ===> ",stopwords_removed_texts)
-    print("clean3_text ==",clean3_text)
-    return clean3_text
-    
+    print("전처리 텍스트 ==",compoundWords_removed_texts)
+    return compoundWords_removed_texts
+
 def get_symptom_score(keywords, fasttext_model):
     total_score = 0
     input_category_counts = {"category1": 0, "category2": 0, "category3": 0, "category4": 0, "category5": 0, "category6": 0}
 
     for word in keywords:
-            max_similar_symptom = None
-            max_similarity = 0
-            for category, symptoms in symptom_scores.items(): # items()메서드 : key-value쌍으로 반환
-                for symptom, score in symptoms.items():
-                    if word not in fasttext_model.wv or symptom not in fasttext_model.wv:
-                        continue
+        max_similar_symptom = None
+        max_similarity = 0
+        for category, symptoms in symptom_scores.items(): 
+            for symptom, score in symptoms.items():
+                if word not in fasttext_model.wv or symptom not in fasttext_model.wv:
+                    continue
 
-                    similarity = 1 - cosine(fasttext_model.wv[word], fasttext_model.wv[symptom])
-                    if similarity > max_similarity:
-                        max_similarity = similarity
-                        max_similar_symptom = symptom
-                        current_category = category
-                        current_score = score
+                similarity = 1 - cosine(fasttext_model.wv[word], fasttext_model.wv[symptom])
+                if similarity > max_similarity:
+                    max_similarity = similarity
+                    max_similar_symptom = symptom
+                    current_category = category
+                    current_score = score
 
-            if max_similarity >= 0.97:
-                input_category_counts[current_category] += 1
-                if input_category_counts[current_category] >= 2:
-                    additional_score = 2 * (input_category_counts[current_category])
+        if max_similarity >= 0.988:
+            input_category_counts[current_category] += 1
+            if input_category_counts[current_category] >= 2:
+                additional_score = 2 * sum(input_category_counts.values())
+            total_score += current_score
+        else:
+            additional_score = 0
 
-                else:
-                    additional_score = 0
-
-                total_score += current_score + additional_score
-
-                # 유사도 분석을 위한 출력
-    print("최종점수==",total_score)
+        print(f"키워드: {word}, 매칭 증상: {max_similar_symptom}, 점수: {current_score}, 유사도: {max_similarity}")
+    
+    total_score += additional_score
     return total_score
 
 def main():
@@ -72,8 +67,8 @@ def main():
     symptom_score = get_symptom_score(preprocessed_words, fasttext_model)
     severity_level = get_severity_level(symptom_score)
 
-    print(f"환자의 중증도 레벨은 {severity_level} 입니다.")
+    print(f"총 점수: {symptom_score}, 중증도 레벨: {severity_level} 입니다.")
 
-# #스위치
-# if __name__ == '__main__':
-#     main()
+# # 스위치
+if __name__ == '__main__':
+    main()
--- yongjae_word/model_word.py
+++ yongjae_word/model_word.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 # ↑상단의 주석 지우지 마세요. python이 읽는 주석입니다.
-
-# import time
+import time
 from konlpy.tag import Okt
 from multiprocessing import freeze_support
 from gensim.models.fasttext import FastText
@@ -9,10 +8,13 @@ import re
 from gensim.models import KeyedVectors
 import numpy as np
 from tqdm import tqdm
-
+import random
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from stop_word import stop_words,compound_words
 
 okt = Okt()
-stopwords = ['의','면서','별','부분은','보임','씩','증도','닮다','다','없다','고','엔','힘들다','액체','까지','두','야하다','오늘','아침','오늘아침','커지다','급격하다','치가','나타나다','조','이상승','이송이','병원','력','지속','함피','함','즉시','집중','떨어지다','하고','심하다','인','적','며','필요하다','되어다','되었다','인하다','심해지다','계속','있다','심각하다','점점','때문','을','로','상','하','지다','않다','이다','되다','에서','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다','아침','또','또한','가끔','감','코',"계,'속",'현재','이어진','상황']
+#stopwords파일 따로 생성하기
 ReToken_texts = []
 clean_texts = []
 
@@ -25,24 +27,25 @@ def preprocess(data):
     ReToken_texts = []
     for line in data:
         line = re.compile('[^가-힣]').sub(' ', line)
-        token_text = okt.nouns(line)
-        stopwords_removed_texts = [word for word in token_text if not word in stopwords]
-        if stopwords_removed_texts:
-            ReToken_texts.append(stopwords_removed_texts)
+        token_text = okt.morphs(line)
+        stopWords_removed_texts = [word for word in token_text if not word in stop_words]
+        compoundWords_removed_texts = [word for word in stopWords_removed_texts if not word in compound_words]
+        # print("전처리 완료==>",compoundWords_removed_texts)
+        if compoundWords_removed_texts:
+             ReToken_texts.append(compoundWords_removed_texts)
     return ReToken_texts
 
-#모델 초기화 & 초기 학습용    
+#모델 초기화 & 초기 학습용
+
 def train_fasttext_model(texts):
     print("-" * 20)
     print("학습 시작")
-    model = FastText(vector_size=100, window=3, min_count=3, workers=3, sg=1, epochs=700)
+    model = FastText(vector_size=100, window=5, min_count=6, workers=4, sg=1)
 
     model.build_vocab(texts)
-    # model.train(texts, total_examples=len(texts), epochs=700)
 
-    # tqdm진행률
-    with tqdm(total=700) as pbar:
-        for epoch in range(700):
+    with tqdm(total=1500) as pbar:
+        for epoch in range(1000):
             model.train(texts, total_examples=len(texts), epochs=1)
             model.alpha -= 0.001
             model.min_alpha = model.alpha
@@ -54,26 +57,47 @@ def train_fasttext_model(texts):
     print("학습 및 저장 완료")
     print("-" * 20)
 
-#누적 학습용    
+#누적 학습용
 def update_fasttext_model(texts):
     print("-" * 20)
-    print("모델 업데이트 시작")
+    print("학습 업데이트 시작")
 
     model = FastText.load(model_path)
+    model.window = 5
+    model.min_count = 6
+    model.workers = 4
     model.build_vocab(texts, update=True)
-    model.train(texts, total_examples=model.corpus_count, epochs=350)
+    model.train(texts, total_examples=model.corpus_count, epochs=1000)
 
-    with tqdm(total=500) as pbar:
-        for epoch in range(500):
+    with tqdm(range(1500)) as pbar:
+        for epoch in pbar:
             model.train(texts, total_examples=model.corpus_count, epochs=1)
             model.alpha -= 0.001
             model.min_alpha = model.alpha
-            pbar.update(1)
+            # pbar.update(1)
 
     model.save(model_path)
     np.save(vectors_ngrams_path, model.wv.vectors_ngrams, allow_pickle=False)
+    model.wv.vectors_ngrams = np.load(vectors_ngrams_path, allow_pickle=False)
+
     print("모델 업데이트가 완료되었습니다.")
     print("-" * 20)
+
+# ##그래프화
+# def visualize_similarity(model):
+#     word_list = list(model.wv.key_to_index)
+#     random.shuffle(word_list)
+#     selected_words = word_list[:200]
+
+#     vectors = np.array([model.wv[word] for word in selected_words])
+#     tsne = TSNE(n_components=2, random_state=0)
+#     vectors_2d = tsne.fit_transform(vectors)
+
+#     plt.figure(figsize=(12, 12))
+#     for point, word in zip(vectors_2d, selected_words):
+#         plt.scatter(point[0], point[1])
+#         plt.annotate(word, xy=(point[0], point[1]))
+#     plt.show()
 
 def main():
     freeze_support()
@@ -83,15 +107,16 @@ def main():
     print(f"읽은 행 수: {len(data)}")
 
     ReToken_texts = preprocess(data)
-    train_fasttext_model(ReToken_texts)
+    # train_fasttext_model(ReToken_texts) ##새학습
     # if not os.path.exists(model_path):
     #     train_fasttext_model(ReToken_texts)
     # else:
-    #     update_fasttext_model(ReToken_texts)
-    
-    model = KeyedVectors.load(model_path)
-    model.wv.vectors_ngrams = np.load(vectors_ngrams_path, allow_pickle=False)
+    update_fasttext_model(ReToken_texts) ##누적학습
 
-# ##스위치
+    # model = KeyedVectors.load(model_path) #삭제해도될지도?
+    # model.wv.vectors_ngrams = np.load(vectors_ngrams_path, allow_pickle=False) #삭제해도될지도?
+    # visualize_similarity(model)
+
+#스위치
 # if __name__ == '__main__':
-#     main()
+#         main()
